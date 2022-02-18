35cee3c7550fa27251705ce0f06011f1aff07d0a
haeretik <haeretik@gmail.com>
2022-02-18 12:18:10 -0600

    problem 3

---
+++ 2.1_arearectangle.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+#Area of a Rectangle
+a = float(input("Enter Width: "))
+b = float(input("Enter Length: "))
+area = b*a
+print(f"Area= {area}")
+
---
+++ 3_nameuser.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+# Name and Age from the user
+
+name = input(f"Enter your name: ")
+age = input(f"Enter your age: ")
+add = input(f"Enter your address: ")
+marks = input(f"Enter your marks: ")
+print(f"\nHi {name}! your age is {age}.\nYour address is {add} and your marks is {marks}.")
