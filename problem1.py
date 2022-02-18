3f6135d30eb7d9fcf9103b9792d2af001cecb97f
haeretik <haeretik@gmail.com>
2022-02-18 09:39:44 -0600

    Create proyect

---
+++ problem1.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+num= int (input("Numero de Iteraciones?"))
+print(f"Number \t\t Square")
+
+for n in range(num+1):
+    print(f"{n}\t\t{n*n}")
+    n+=1
+
+
+#print("Number \t\t Square")
+#print("1 \t\t "+str(1*1))
+#print("2 \t\t "+str(2*2))
+#print("3 \t\t "+str(3*3))
+#print("4 \t\t "+str(4*4))
+#print("5 \t\t "+str(5*5))
