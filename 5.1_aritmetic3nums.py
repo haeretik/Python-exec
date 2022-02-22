2bfc4ee3289338592d2f7f85dd3c924e06442ffc
haeretik <haeretik@gmail.com>
2022-02-22 12:51:31 -0600

    problem 5

---
+++ 5.1_aritmetic3nums.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+# perform arithmatic operation on two number
+
+a = int(input(f"Enter one  number: "))
+b = int(input(f"Enter 2nd number: "))
+c = int(input(f"Enter 3th number: "))
+
+print(f"Addition of two numer = {str(a+b+c)}")
+print(f"Subtraction of two number = {str(a-b-c)}")
+print(f"Multiplication of two number = {str(a*b*c)}")
+print(f"Division of two number = {str(a/b/c)}")
+print(f"Modulus of two number = {str(a%b%c)}")
+
---
+++ 5_aritmetic.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+# perform arithmatic operation on two number
+
+a = int(input(f"Enter one  number: "))
+b = int(input(f"Enter 2nd number: "))
+print(f"Addition of two numer = {str(a+b)}")
+print(f"Subtraction of two number = {str(a-b)}")
+print(f"Multiplication of two number = {str(a*b)}")
+print(f"Division of two number = {str(a/b)}")
+print(f"Modulus of two number = {str(a%b)}")
+
