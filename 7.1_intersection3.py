3789f6a15e022f86e927586f5b56e394fc12df56
Haeretik <haeretik@gmail.com>
2022-04-06 10:43:50 -0500

    problem7

---
+++ 7.1_intersection3.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+a={5,2,4,6,7,1}
+b={5,3,11}
+c={4,5,12,2,1,0}
+
+d= a.intersection(b,c)
+
+print(f"{d}")
---
+++ 7_intersection2.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+# find intersection of two 
+
+# a = {12,4,5,7,8,9}
+# b = {1,12,33,8,0,9}
+
+# c = a.intersection(b)
+a={12,1,2,4,5,7,8,9}
+b={1,2,33,44,12}
+
+c= a.intersection(b)
+
+print(f"Intersection of Two SET")
+print(f"{c}")
+
