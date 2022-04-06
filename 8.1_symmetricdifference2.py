5183febdbde992266ff829dc965c6fb90a918649
Haeretik <haeretik@gmail.com>
2022-04-06 11:19:09 -0500

    problem 8

---
+++ 8.1_symmetricdifference2.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+# print symmetric difference of two set
+
+a={12,2,0,3,8}
+b={15,10,12,3,6}
+
+c=a.symmetric_difference(b)
+
+print(f"Symmetric Diference of two set")
+
+print(f"{c}")  
+
+# a-b ? no of a that aren't present in the b set
+# b-a ? no of b that aren't present in the a set
+
---
+++ 8_difference2.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+# print difference of two set
+
+a={23,5,1,12,4,6,7}
+b={6,11,12,4,5,2,15,21,22,33}
+
+c=b.difference(a)
+
+print(f"Difference of two set")
+
+print(f"{c}")  
+
+# a-b ? no of a that aren't present in the b set
+# b-a ? no of b that aren't present in the a set
+
